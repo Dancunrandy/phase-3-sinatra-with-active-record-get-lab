aed0e2bb64ba0216f130e6ba6d6bd64c89e45f2a
dancunrandy <dancunrandy@gmail.com>
2023-03-01 14:19:27 +0300

    commit tree 68cff43fd9dccc7347bf79d17ff2e3c41cb60d94

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,47 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  configure do
+    set :views, 'app/views'
+    set :public_dir, 'public'
+    set :database, { adapter: "sqlite3", database: "db/bakery.db" }
+    set :show_exceptions, false
+  end
 
+  not_found do
+    status 404
+    erb :error
+  end
+
+  error do
+    status 500
+    erb :error
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/bakeries' do
+    content_type :json
+    bakeries = Bakery.all
+    bakeries.to_json(include: :baked_goods)
+  end
+
+  get '/bakeries/:id' do
+    content_type :json
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    content_type :json
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    content_type :json
+    baked_good = BakedGood.order(price: :desc).first
+    baked_good.to_json
+  end
 end
